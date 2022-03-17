4a1d6008b0709d1050257cd53df32e3418f0f37f
hsbin316 <hsbin316@gmail.com>
2022-03-17 15:39:24 +0900

    app.js  commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
-import dog from './dog.jpg';
+import faker from '@faker-js/faker';
+import faker_ko from '@faker-js/faker/locale/ko';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 import './App.css';
 
 function App() {
-  const h1Element = <h1>H1 제목 태그 입니다.</h1>;
-  const imgElement = <img src={dog} className="App-logo" alt="dog" />;
+  const userDatas = [];
+  
+  while(userDatas.length < 50) {
+    userDatas.push({
+      avatar: faker.image.avatar(),
+      name: `${faker_ko.name.lastName()}${faker_ko.name.firstName()}`,
+      email: faker.internet.email(),
+      jobTitle: faker.name.jobTitle(),
+      phoneNo: faker_ko.phone.phoneNumber()
+    })
+  }
 
-  const testData = [
-    {
-      text : "정기회의 회기는 100일을, 임시회의 회기는 30일을 초과할 수 없다.",
-      imgUrl: "adfasdfasdfsdfsaf",
-    },{
-      text: "헌법개정안이 제2항의 찬성을 얻은 때에는 헌법개정은 확정되며, 대통령은 즉시 이를 공포하여야 한다.",
-      imgUrl: "adfasdfasdfadsf"
-    },{
-      text: "국회는 헌법개정안이 공고된 날로부터 60일 이내에 의결하여야 하며, 국회의 의결은 재적의원 3분의 2 이상의 찬성을 얻어야 한다.",
-      imgUrl: "asdfasdfadfsdf"
-    }
-  ]
+
+  const userCards = userDatas.map((userData, idx) => {
+    return <div key={idx}>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="300"
+            image={userData.avatar }
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {userData.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              { userData.jobTitle }
+              { userData.email }<br />
+              { userData.phoneNo} 
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
+    
+    
+    
+    // <div key={idx}>
+    //   <h4>{ userData.jobTitle }</h4>
+    //   <img src={userData.avatar } alt="사용자 프로필용 아바타"></img>
+    //   <h5>{ userData.name }</h5>
+    //   { userData.email }<br />
+    //   { userData.phoneNo} 
+    // </div>
+  })
+
+  console.log(userDatas)
   return (
     <div className="App">
-      <header className="App-header">
-        { h1Element }
-        { imgElement }
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      {userCards}
     </div>
   );
 }
---
+++ src/DogMain.js
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker';
+import dog from './dog.jpg';
+
+  const testData = [
+    {
+      text : "정기회의 회기는 100일을, 임시회의 회기는 30일을 초과할 수 없다.",
+      imgUrl: "https://i.natgeofe.com/n/3faa2b6a-f351-4995-8fff-36d145116882/domestic-dog_16x9.jpg",
+    },{
+      text: "헌법개정안이 제2항의 찬성을 얻은 때에는 헌법개정은 확정되며, 대통령은 즉시 이를 공포하여야 한다.",
+      imgUrl: "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d"
+    },{
+      text: "국회는 헌법개정안이 공고된 날로부터 60일 이내에 의결하여야 하며, 국회의 의결은 재적의원 3분의 2 이상의 찬성을 얻어야 한다.",
+      imgUrl: "https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=0d3f33fb6aa6e0154b7713a00454c83d"
+    }
+  ]
+
+function DogMain() {
+    const h1Element = <h1>H1 제목 태그 입니다.</h1>;
+    const imgElement = <img src={dog} className="App-logo" alt="dog" />;
+    return (
+        <>
+            { h1Element }
+            { imgElement }
+            <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+            {testData.map((contents)=>{
+                return <div>
+                    <img src={faker.image.avatar()} alt="avatar"></img>
+                    {contents.text}
+                    <img src={faker.image.avatar()} alt="avata"></img>
+                </div>
+            })}
+        </>
+    );
+}
+
+export default DogMain;
